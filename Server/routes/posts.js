00e2462446e31a1fcc0fcc3c69da582eecb3b6ba
A-Amiin <ahmed.m.aminn@gmail.com>
2024-12-12 20:30:01 +0200

    Edit on the Post model & Create the Post API in the Posts Collection

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require("../Utilities/utilities");
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => res.send("posts route"));
+router.post("/",
+    [
+        auth,
+        check("text", "text is required").notEmpty(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select("-password");
+            const newPost = new Post(
+                {
+                    text: req.body.text,
+                    name: user.name,
+                    user: req.user.id
+                }
+            );
+            const post = await newPost.save();
+            res.json(post);
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server Error");
+        }
+    }
+);
 
 module.exports = router;
