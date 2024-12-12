f91cab5b3acb2284e9f0f30913e34e86e641ffeb
A-Amiin <ahmed.m.aminn@gmail.com>
2024-12-12 17:16:43 +0200

    add the post API for profiles

--- Server/routes/profiles.js
+++ Server/routes/profiles.js
@@ -50,40 +50,4 @@ router.post('/', Utilities.auth,
     }
 );
 
-
-router.get('/me', Utilities.auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name']);
-        if (!profile) return res.status(400).json({ msg: "No profile found for this user" });
-        return res.json(profile);
-    } catch (err) {
-        console.error("Error fetching profile:", err.message);
-        res.status(500).send("Server error");
-    }
-});
-
-
-router.get('/', Utilities.auth, async (req, res) => {
-    try {
-        const profiles = await Profile.find().populate('user', ['name']);
-        return res.json(profiles);
-    } catch (err) {
-        console.error("Error fetching profile:", err.message);
-        res.status(500).send("Server error");
-    }
-});
-
-
-router.get('/users/:user_id', Utilities.auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name']);
-        if (!profile) return res.status(400).json({ msg: "No profile found for this user" });
-        return res.json(profile);
-    } catch (err) {
-        if (err.kind === 'ObjectId') return res.status(404).json({ msg: "Profile not found" });
-        console.error("Error fetching profile:", err.message);
-        res.status(500).send("Server error");
-    }
-});
-
 module.exports = router;
